343fdcaea8a66d3438001c765acc2b396607ac6f
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-06-04 08:43:16 -0600

    Able to get the record

--- server.js
+++ server.js
@@ -37,15 +37,26 @@ app.get('/', function(req, res) {
   });
 });
   
+//Local variables  
+var my_token = '';
+var get_record_msg = {
+			host : 'pub.sandbox.orcid.org', 
+			port : 443,
+			path : '/v1.2/[orcid]/orcid-profile', 
+			method : 'GET', // do GET
+			headers: {				
+				'Accept': 'application/vnd.orcid+xml'
+			} // We will need to add the authorization header here, like this: 'Authorization': 'Bearer [bearer]',
+		};
   
 // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
 app.get('/get-auth', function(req, res) {
-	console.log("This is working")
-  // Prepare the context
-  res.redirect(authorization_uri);
+	console.log('This is working')
+	// Prepare the context
+	res.redirect(authorization_uri);
 });
 
-//curl -H "Accept: application/orcid+xml" "http://pub.sandbox.orcid.org/v2.0_rc1/0000-0002-3373-1120/activities"
+//curl -H 'Accept: application/orcid+xml' 'http://pub.sandbox.orcid.org/v2.0_rc1/0000-0002-3373-1120/activities'
 // Get the access token object (the authorization code is given from the previous step).
 app.get('/callback', function(req, res) {
   var token;
@@ -54,48 +65,42 @@ app.get('/callback', function(req, res) {
     code: code,
     redirect_uri: 'http://localhost:8000/callback'
   }, function(error, result){
-    token = oauth2.accessToken.create(result);
+    my_token = oauth2.accessToken.create(result);
     // TODO: save token here, to be read by show-work later
-	console.log("Token is: " + JSON.stringify(token));	
-	//res.redirect('http://localhost:8000/show-work');	
-	
-	var optionsgetmsg = {
-			host : 'pub.sandbox.orcid.org', // here only the domain name    
-			port : 443,
-			path : '/v2.0_rc1/0000-0001-6442-2876/activities', // the rest of the url with parameters if needed
-			method : 'GET', // do GET
-			headers: {
-				'Authorization': 'Bearer 782c0d26-68ca-4c4b-b484-105bf5871760',
-				'Accept': 'application/vnd.orcid+xml'
-			}
-		};
-	
-	console.info('Options prepared:');
-	console.info(optionsgetmsg);
-	console.info('Do the GET call');
-	
-	var reqGet = https.request(optionsgetmsg, function(res) {
-		console.log("statusCode: ", res.statusCode);
-		// uncomment it for header details
-		//  console.log("headers: ", res.headers);
- 
-		res.on('data', function(d) {
-			console.info('GET result after POST:\n');
-			process.stdout.write(d);
-			console.info('\n\nCall completed');
-		});		
-	});
-	
-	reqGet.end();
-	reqGet.on('error', function(e) {
-		console.error(e);
-	});
-	
+	console.log('my_token saved');	
+	res.redirect('http://localhost:8000/show-token');		
   });
 });
 
 
 // TODO: make show-work page
+app.get('/show-token', function(req, res) {
+	console.log('Token is: ' + JSON.stringify(my_token));	
+	res.redirect('http://localhost:8000/get-record');
+});
+
+app.get('/get-record', function(req, res){
+	//Add bearer header
+	get_record_msg.headers['Authorization'] = 'Bearer ' + my_token.token.access_token;
+	//Replace the orcid placeholder in the path
+	get_record_msg.path = get_record_msg.path.replace('[orcid]', my_token.token.orcid);
+	console.log('Request will be:' + JSON.stringify(get_record_msg))
+	
+	var req_get_record = https.request(get_record_msg, function(res) {
+		console.log("statusCode: ", res.statusCode);		
+		res.on('data', function(d) {
+			console.info('GET result after POST:\n');
+			process.stdout.write(d);
+			console.info('\n\nCall completed');
+		});
+		res.on('error', function(e){
+			console.error(e);
+		});
+	});
+	
+	req_get_record.end();		
+});
+
 
 
 app.listen(port, function (){
